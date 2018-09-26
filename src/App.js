e5d3a12732b91136e5e6c5c66bb0dea21d11c5b1
Joonhee <pakjoonhee90@gmail.com>
2018-09-25 23:12:55 -0700

    creating the form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
+  state = {
+    goingTo: "",
+    checkIn: "",
+    checkOut: "",
+    Rooms: "",
+    Adults: "",
+    Children: "",
+    addAFlight: "",
+    addACar: ""
+  }
+
+  change = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <input 
+            name="goingTo"
+            placeHolder="Destination, hotel name, airport, train station, landmark, or address" 
+            value={this.state.goingTo} 
+            onChange={e => this.change(e)} 
+          />
+        <br />
+        <Button color="danger">Search</Button>
+        </form>
       </div>
     );
   }
